bfc89204eedcb6b398675205fc747987d284b32c
punitjain-jp <76389204+punitjain-jp@users.noreply.github.com>
2022-03-08 01:22:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ def desc_calc():
     nmol = desc.shape[0]
     ndesc = desc.shape[1]
     def header():
-        st.markdown(f'<p style="background-color:#076931;color:#f2eae4;font-size:24px;border-radius:6%;">{st.info('Selected fingerprint: ' + user_fp)
-    st.info('Number of molecules: ' + str(nmol))
-    st.info('Number of descriptors: ' + str(ndesc-1))}</p>', unsafe_allow_html=True)
+        st.markdown(f'<p style="background-color:#076931;color:#f2eae4;font-size:24px;border-radius:6%">{st.info("Selected fingerprint: " + user_fp)
+    st.info("Number of molecules: " + str(nmol))
+    st.info("Number of descriptors: " + str(ndesc-1))}</p>', unsafe_allow_html=True)
 
     os.remove('molecule.smi')
 
